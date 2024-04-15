dd65122971189567f5a2c04a35b3cc4c6d086275
Lavyatarah <mattlavyy@gmail.com>
2024-04-15 19:30:14 +0300

    pettycash

--- app/models.py
+++ app/models.py
@@ -1,11 +1,19 @@
-# from . import db
-from sqlalchemy import Column, Integer, String, Float, DateTime
 from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
 
-class Transaction():
+db = SQLAlchemy()
+
+class Transaction(db.Model):
     __tablename__ = 'transaction'  # Specify the table name explicitly
-    id = Column(Integer, primary_key=True)
-    date = Column(DateTime, default=datetime.utcnow)
-    description = Column(String(100), nullable=False)
-    amount = Column(Float, nullable=False)
-    category = Column(String(50), nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+    description = db.Column(db.String(100), nullable=False)
+    amount = db.Column(db.Float, nullable=False)
+    category = db.Column(db.String(50), nullable=False)
+
+def calculate_balance():
+    transactions = Transaction.query.all()
+    balance = 0
+    for transaction in transactions:
+        balance += transaction.amount
+    return balance
--- app/routes.py
+++ app/routes.py
@@ -1,22 +1,34 @@
-# #!/usr/bin/python3
+#!/usr/bin/python3
 
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
-from models import Transaction
+from models import Transaction, calculate_balance
+import datetime
+from models import db
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///pettycash.db'
-db = SQLAlchemy(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://lavy:Lavenia1@localhost/pettycash'
+app.secret_key = os.getenv('SECRET_KEY')
+db.init_app(app)
 
 @app.route('/')
 def index():
-    """transactions = Transaction.query.order_by(Transaction.date.desc()).all()"""
-    return render_template('index.html')
+    transactions = Transaction.query.order_by(Transaction.date.desc()).all()
+    balance = calculate_balance()
+    return render_template('index.html', transactions=transactions, balance=balance)
 
 @app.route('/add_transaction', methods=['GET', 'POST'])
 def add_transaction():
     if request.method == 'POST':
-        date = request.form['date']
+        date_str = request.form['date']
+        
+        date = datetime.datetime.strptime(date_str, '%Y-%m-%d').date()
         description = request.form['description']
         amount = request.form['amount']
         category = request.form['category']
@@ -31,9 +43,13 @@ def add_transaction():
 
         new_transaction = Transaction(date=date, description=description, amount=amount, category=category)
         db.session.add(new_transaction)
-        db.session.commit()
 
-        return redirect(url_for('index'))
+        try:
+            db.session.commit()
+            flash("Transaction Successfully Created")  # Pass a string message to the flash function
+        except Exception as e:
+            db.session.rollback()
+            return "Error in Adding Transaction: " + str(e)
 
     return render_template('add_transaction.html')
 
@@ -46,24 +62,26 @@ def search():
 @app.route('/filter', methods=['GET'])
 def filter():
     category = request.args.get('category')
-    start_date = request.args.get('start_date')
-    end_date = request.args.get('end_date')
+    start_date= request.args.get('start_date')
+    end_date= request.args.get('end_date')
+    balance = calculate_balance()
 
     query = Transaction.query
-
     if category:
         query = query.filter_by(category=category)
 
     if start_date and end_date:
         try:
-            start_date = datetime.strptime(start_date, '%Y-%m-%d')
-            end_date = datetime.strptime(end_date, '%Y-%m-%d') + datetime.timedelta(days=1)
+            start_date = datetime.datetime.strptime(start_date, '%Y-%m-%d')
+            end_date = datetime.datetime.strptime(end_date, '%Y-%m-%d') + datetime.timedelta(days=1)
             query = query.filter(Transaction.date >= start_date, Transaction.date < end_date)
         except ValueError:
             return "Error: Invalid date format. Please use YYYY-MM-DD."
 
     transactions = query.order_by(Transaction.date.desc()).all()
-    return render_template('index.html', transactions=transactions)
+    return render_template('index.html', transactions=transactions, balance=balance)
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
